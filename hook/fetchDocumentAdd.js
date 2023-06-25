b505f148375227b79dfc261086e285d8d9d8cefd
Wojciech Korniak <wojciech.korniak@goldenberry.eu>
2023-06-25 22:45:18 +0200

    fix adding documents

--- hook/fetchDocumentAdd.js
+++ hook/fetchDocumentAdd.js
@@ -1,40 +1,28 @@
-import { supabase } from '../lib/supabase/supabase'
+import { supabase } from "../lib/supabase/supabase";
 
 async function fetchDocumentAdd(userId, documentType, valueType, value) {
-    const insertPassed = await insertDocumentObject(userId, documentType)
-    if (!insertPassed) return false;
-    const documentId = await getDocumentId(userId, documentType)
-    if (!documentId) return false;
-    const { error } = await supabase
-        .from('document_data_value')
-        .insert([
-            {data_name: valueType, document_id: documentId, value: value},
-        ])
-    if (error) {
-        console.error('Nie można dodać dokumentu', error);
-        return false;
-    }
-    return true;
-};
-
-async function insertDocumentObject(userId, documentType) {
-    const { error } = await supabase
-        .from('document_object')
-        .insert([
-            { type_name: documentType, user_id: userId }
-        ])
-    if (error) return false;
-    return true;
+  const document = await insertDocumentObject(userId, documentType);
+  if (!document) return false;
+  const { error } = await supabase
+    .from("document_data_value")
+    .insert([
+      { data_name: valueType, document_id: document.document_id, value: value },
+    ]);
+  if (error) {
+    console.error("Nie można dodać dokumentu", error);
+    return false;
+  }
+  return true;
 }
 
-async function getDocumentId(userId, documentType) {
-    const { data, error } = await supabase
-        .from('document_object')
-        .select(`document_id`)
-        .eq('user_id', userId)
-        .eq('type_name', documentType)
-    if (error) return false;
-    return data;
+async function insertDocumentObject(userId, documentType) {
+  const { error, data } = await supabase
+    .from("document_object")
+    .insert([{ type_name: documentType, user_id: Number(userId) }])
+    .select("document_id")
+    .single();
+  if (error) return false;
+  return data;
 }
 
 export default fetchDocumentAdd;
--- hook/index.js
+++ hook/index.js
@@ -1,9 +1,17 @@
-
 import fetchRentals from "./fetchRentals";
-import fetchRentBike from "./fetchRentBike"
+import fetchRentBike from "./fetchRentBike";
 import fetchBikeReturn from "./fetchBikeReturn";
 import fetchCarReturn from "./fetchCarReturn";
 import fetchRentCar from "./fetchRentCar";
 import fetchRentalsCar from "./fetchRentalsCar";
+import fetchDocumentAdd from "./fetchDocumentAdd";
 
-export { fetchRentals, fetchRentBike, fetchBikeReturn, fetchCarReturn, fetchRentCar, fetchRentalsCar }
+export {
+  fetchRentals,
+  fetchRentBike,
+  fetchBikeReturn,
+  fetchCarReturn,
+  fetchRentCar,
+  fetchRentalsCar,
+  fetchDocumentAdd,
+};
